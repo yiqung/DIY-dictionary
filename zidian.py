36fced07eb1382e60844deeb1a5c5b7908e25676
yuqing <1403801697@qq.com>
2016-06-17 14:52:39 +0800

    Update zidian.py

--- zidian.py
+++ zidian.py
@@ -1,17 +1,24 @@
 from tkinter import*
 import os
 os.chdir('words')
-#=======================================函数==========================================================
+root=Tk()
+root.title('自典')
+root.resizable(width=False,height=False)
+menubar=Menu(root,font='微软雅黑')
+root.config(menu=menubar)
+input_word=Entry(root,font='微软雅黑',borderwidth=0)
+input_word.grid(row=0,columnspan=4)
+alphabet=Listbox(root,borderwidth=0,font='微软雅黑')
+alphabet.grid(row=1,columnspan=4)
 
+#=============================================================================================
 def add_button():
-        
-
+    
     add=Tk()
     add.title('添加')
 
     Label(add,text='单词:',font='微软雅黑').grid(row=0)
     Label(add,text='解释:',font='微软雅黑').grid(row=1)
-        
         
     add_word=Entry(add,borderwidth=0,font='微软雅黑')
     add_word.grid(row=0,column=1)
@@ -19,10 +26,10 @@ def add_button():
     add_explain=Entry(add,borderwidth=0,font='微软雅黑')
     add_explain.grid(row=1,column=1)
         
-        
-
     def show():
         word=str(add_word.get())
+        explain=str(add_explain.get())
+        
         Word=word.capitalize()
         list_Words=list(Word)
         frist=list_Words[0]
@@ -30,25 +37,23 @@ def add_button():
         f=open(file)
         str_word=f.read()
         list_word=str_word.split(';\n')
+        list_all=[]
         for each in list_word:
             list_each=each.split(':')
-            if word==list_each[0]:
-                existword=Toplevel()
-                existword.title('提示')
-                existword.geometry('200x50+200+50')
-                Label(existword,text='单词已存在!',font='微软雅黑').grid()
-                break
-            else:
-                expain=str(add_explain.get())
-                word=word+':'+expain
-                f=open(file,'a')
-                f.write(word+';\n')
-            f.close()
-            add_word.delete(0,END)
-            add_explain.delete(0,END)
-         
+            list_all.append(list_each[0])
+        if word in list_all:
+                exsit=Toplevel()
+                Label(exsit,text='单词已存在!',font='微软雅黑').grid()
+        else :
+            word=word+':'+explain
+            f=open(file,'a')
+            f.write(word+';\n')
+        f.close()
+        add_word.delete(0,END)
+        add_explain.delete(0,END)
+           
     Button(add,text='添加',borderwidth=0,font='微软雅黑',command=show).grid(row=3,column=1,sticky=E)
-       
+addword=menubar.add_command(label='add',font='微软雅黑',command=add_button)
 #==================================================================================================    
 def search_word(event):
 
@@ -70,95 +75,66 @@ def search_word(event):
             Label(show_word,text=list_each_word[1],font='微软雅黑').grid()
                
     f.close()
-    #=====================================================
-
-        
-        
-    #==========================================================
-def button_word(event):
-    def delete_button():
-     
-        word=listword.get(ACTIVE)
-    
-        f=open(word[0]+'.txt')
-        wordstr=f.read()
-      
-        wordstr=wordstr.replace(word+';\n','')
-    
-        f=open(word[0]+'.txt','w')
-        f.write(wordstr)
-        f.close()
-    word=str(alphabet.get(ACTIVE))
-    word=str(word[0])
-
-    f=open(word+'.txt')
-    wordstr=f.read()
-
-    word_button=Toplevel()    
-    word_button.title(word)
- 
-    menubar=Menu(word_button,font='微软雅黑')
-        
-    word_button.config(menu=menubar)
-    addword=menubar.add_command(label='add',font='微软雅黑',command=add_button)
-        
-    deleteword=menubar.add_command(label='delete',font='微软雅黑',command=delete_button)
-        
-    listword=Listbox(word_button,font='微软雅黑')
-    listword.pack()
-    wordlist=wordstr.split(';\n')
-    for each in wordlist:
-        listword.insert(END,each)
-        alphabet.update_idletasks()
-            
-    f.close()
-#=============================================页面===================================================================
-  
-root=Tk()
-root.title('自典')
-root.resizable(width=False,height=False)
-#===============================================添加=================================================================
-menubar=Menu(root,font='微软雅黑')
-root.config(menu=menubar)
-addword=menubar.add_command(label='add',font='微软雅黑',command=add_button)
-
-#=============================================搜索====================================================================
-
-input_word=Entry(root,font='微软雅黑',borderwidth=0)
-input_word.grid()
-
 input_word.bind('<KeyPress-Return>',search_word)
+#===========================
+def alphabet_list():
+    def word_list():
+        def delete_word():
+            word=alphabet.get(ACTIVE)
+            f=open(word[0]+'.txt')
+            word_str=f.read()
+            word_list=word_str.split(';\n')
+            if word in word_list:
+                word_list.remove(word)
+                f=open(word[0]+'.txt','w')
+                for each in word_list:
+                    if each!='':
+                        f.write(each+';\n')
+                f.close()
+#=================================
+        word=str(alphabet.get(ACTIVE))
+        word=str(word[0])
+        alphabet.delete(0,END)
+        f=open(word+'.txt')
+        wordstr=f.read()
+        wordlist=wordstr.split(';\n')
+        for each in wordlist:
+            alphabet.insert(END,each)
+        f.close()
+        
+        delete_button=Button(root,text='删除',font='微软雅黑',borderwidth=0,command=delete_word)
+        delete_button.grid(row=2,columnspan=4,sticky=W+E)
+        
+        return_button=Button(root,text='返回',font='微软雅黑',borderwidth=0,command=alphabet_list)
+        return_button.grid(row=2,column=0,sticky=W)
+                
+        flash1_button=Button(root,text='刷新',font='微软雅黑',borderwidth=0,command=word_list)
+        flash1_button.grid(row=2,column=3,sticky=E)
+#==================================
+    list_alphabet=os.listdir('.')
+    word_count=0
+    alphabet.delete(0,END)
+    for each_file in list_alphabet:
+        file_name=each_file.split('.')
+        frist=file_name[0]
+        f=open(frist+'.txt')
+        str_all_word=f.read()
+        word_count=str_all_word.count (':')
+        list_all_word=str_all_word.split(';')
+        for each_word in list_all_word:
+            if word_count==1:
+                last_word=list_all_word[0]
+            else:
+                last_word=list_all_word.pop()
+        alphabet.insert(END,frist+'-'+last_word+'-'+str(word_count))
+        
+    f.close()
+    open_button=Button(root,text='打开',font='微软雅黑',borderwidth=0,command=word_list)
+    open_button.grid(row=2,column=0,columnspan=2,sticky=W+E)
+    flash2_button=Button(root,text='刷新',font='微软雅黑',borderwidth=0,command=alphabet_list)
+    flash2_button.grid(row=2,column=2,columnspan=2,sticky=W+E)
 
-#===========================================字母表=================================================================
-
-alphabet=Listbox(root,borderwidth=0,font='微软雅黑')
-
-alphabet.grid()
-
-list_alphabet=os.listdir('.')
-
-word_count=0
-
-for each_file in list_alphabet:
-    file_name=each_file.split('.')
-    
-    frist=file_name[0]
-    f=open(frist+'.txt')
-    str_all_word=f.read()
-    word_count=str_all_word.count (':')
-    list_all_word=str_all_word.split(';\n')
-   
-    for each_word in list_all_word:
-
-        last_word=list_all_word.pop()
-   
-    alphabet.insert(END,frist+'    '+last_word+'     '+str(word_count))
-    alphabet.bind('<ButtonRelease>',button_word)
-
-    
-f.close()
-
+#=====================================================
+alphabet_list()
 #================================================================================================================
 root.mainloop ()
-
-
