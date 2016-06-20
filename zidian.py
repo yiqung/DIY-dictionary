73a8fa47a90ebfad312a5fdd19920adf65857e9a
yuqing <1403801697@qq.com>
2016-06-20 10:13:20 +0800

    Update zidian.py

--- zidian.py
+++ zidian.py
@@ -1,17 +1,24 @@
 from tkinter import*
 import os
 os.chdir('words')
+
+#根窗口
 root=Tk()
 root.title('自典')
 root.resizable(width=False,height=False)
+
+#菜单栏
 menubar=Menu(root,font='微软雅黑')
 root.config(menu=menubar)
+
+#搜素框
 input_word=Entry(root,font='微软雅黑',borderwidth=0)
 input_word.grid(row=0,columnspan=4)
+
+#单词表
 alphabet=Listbox(root,borderwidth=0,font='微软雅黑')
 alphabet.grid(row=1,columnspan=4)
-
-#=============================================================================================
+#==================================添加函数===========================================================
 def add_button():
     
     add=Tk()
@@ -53,8 +60,9 @@ def add_button():
         add_explain.delete(0,END)
            
     Button(add,text='添加',borderwidth=0,font='微软雅黑',command=show).grid(row=3,column=1,sticky=E)
-addword=menubar.add_command(label='添加',font='微软雅黑',command=add_button)
-#==================================================================================================    
+    
+addword=menubar.add_command(label='添加',font='微软雅黑',command=add_button)#调用添加函数
+#========================================搜索函数==========================================================    
 def search_word(event):
 
     search=input_word.get()
@@ -73,13 +81,16 @@ def search_word(event):
             show_word=Toplevel()
             show_word.title(search)
             Label(show_word,text=list_each_word[1],font='微软雅黑').grid()
-               
     f.close()
-input_word.bind('<KeyPress-Return>',search_word)
-#===========================
-def alphabet_list():
-    def word_list():
-        def delete_word():
+    
+input_word.bind('<KeyPress-Return>',search_word)#调用搜索函数
+#===============================字母表函数==============================================
+
+def alphabet_list():#一开始的字母表
+    
+    def word_list():#点了打开之后的单词表
+        
+        def delete_word():#删除函数
             word=alphabet.get(ACTIVE)
             f=open(word[0]+'.txt')
             word_str=f.read()
@@ -91,7 +102,7 @@ def alphabet_list():
                     if each!='':
                         f.write(each+';\n')
                 f.close()
-#=================================
+#-----------------------------------------------
         word=str(alphabet.get(ACTIVE))
         word=str(word[0])
         alphabet.delete(0,END)
@@ -102,15 +113,15 @@ def alphabet_list():
             alphabet.insert(END,each)
         f.close()
         
-        delete_button=Button(root,text='删除',font='微软雅黑',borderwidth=0,command=delete_word)
+        delete_button=Button(root,text='删除',font='微软雅黑',borderwidth=0,command=delete_word)#删除按钮
         delete_button.grid(row=2,columnspan=4,sticky=W+E)
         
-        return_button=Button(root,text='返回',font='微软雅黑',borderwidth=0,command=alphabet_list)
+        return_button=Button(root,text='返回',font='微软雅黑',borderwidth=0,command=alphabet_list)#返回按钮
         return_button.grid(row=2,column=0,sticky=W)
                 
-        flash1_button=Button(root,text='刷新',font='微软雅黑',borderwidth=0,command=word_list)
+        flash1_button=Button(root,text='刷新',font='微软雅黑',borderwidth=0,command=word_list)#刷新按钮
         flash1_button.grid(row=2,column=3,sticky=E)
-#==================================
+#---------------------------------------------
     list_alphabet=os.listdir('.')
     word_count=0
     alphabet.delete(0,END)
@@ -127,14 +138,14 @@ def alphabet_list():
             else:
                 last_word=list_all_word.pop()
         alphabet.insert(END,frist+'-'+last_word+'-'+str(word_count))
-        
     f.close()
-    open_button=Button(root,text='打开',font='微软雅黑',borderwidth=0,command=word_list)
+    
+    open_button=Button(root,text='打开',font='微软雅黑',borderwidth=0,command=word_list)#打开按钮
     open_button.grid(row=2,column=0,columnspan=2,sticky=W+E)
-    flash2_button=Button(root,text='刷新',font='微软雅黑',borderwidth=0,command=alphabet_list)
+    
+    flash2_button=Button(root,text='刷新',font='微软雅黑',borderwidth=0,command=alphabet_list)#刷新按钮
     flash2_button.grid(row=2,column=2,columnspan=2,sticky=W+E)
 
-#=====================================================
-alphabet_list()
-#================================================================================================================
+alphabet_list()#调用字母表函数
+#===========================over======================================
 root.mainloop ()
